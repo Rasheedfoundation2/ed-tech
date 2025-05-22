4994e9ac299599576ada35025b71c14b91a0e302
arunvRF <arun@rasheedfoundation.org>
2025-05-22 11:25:49 +0530

    add msg sending and update the chat list ui

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import '../css/Chat.css'
 const Chat = () => {
     
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([
+        { id: 1, text: "Hey, How r u?", type: "received"},
+        { id: 2, text: "I'm fine what abt u?", type: "sent"},
+    ]);
+
+    const sendMessage = () => {
+        const trimmed = input.trim();
+        if(!trimmed) return;
+
+        const newMessage = {
+            id: messages.length + 1,
+            text: trimmed,
+            type: "sent"
+        };
+
+        setMessages([...messages, newMessage]);
+        setInput("");
+    }
+
+    const handleEnter = (e) => {
+        if (e.key === "Enter")
+        {
+            sendMessage();
+        }
+    }
+
     return (
         <>
             <div className="chat-wrapper">
@@ -11,15 +38,22 @@ const Chat = () => {
                     <div className="chat-header">Chat with Demo</div>
 
                     <div className="chat-message-list">
-                        <div className="message received">Hey, how are you?</div>
-                        <div className="message sent">I'm good! What about you?</div>
-                        <div className="message received">Doing great. Ready for the meeting?</div>
-                        <div className="message sent">Yes, almost. Just finishing this task.</div>
+                        {messages.map((msg) => (
+                            <div key={msg.id} className={`message ${msg.type}`}>    
+                                {msg.text}
+                            </div>
+                        ))}
                     </div>
 
                     <div className="chat-footer">
-                        <input type="text" className="chat-input"/>
-                        <button className="chat-btn">Send</button>
+                        <input 
+                            type="text" 
+                            className="chat-input"
+                            value={input}
+                            onChange={(e) => setInput(e.target.value)}
+                            onKeyDown={handleEnter}
+                        />
+                        <button className="chat-btn" onClick={sendMessage}>Send</button>
                     </div>
                 </div>
 
