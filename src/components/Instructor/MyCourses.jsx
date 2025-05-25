5b0fa4bb45e99b5a1ef6ca463025c3fa234a8cf6
arunvRF <arun@rasheedfoundation.org>
2025-05-25 21:51:40 +0530

    add MyCourses component and page for instructor

---
+++ src/components/Instructor/MyCourses.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../css/Instructor/MyCourses.css'
+
+const MyCourses = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default MyCourses;
