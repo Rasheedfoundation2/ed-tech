c1c76ab65cd996409cecc6ef11bc34240c4e775c
arunvRF <arun@rasheedfoundation.org>
2025-05-27 12:59:21 +0530

    add MyCourses component with course details and styling

--- src/components/Instructor/MyCourses.jsx
+++ src/components/Instructor/MyCourses.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import '../../css/Instructor/MyCourses.css'
+import '../../css/Instructor/MyCourses.css';
+import pic from "../../assets/images/courseSlider/js.png";
+import { FaStar, FaUser } from "react-icons/fa";
 
 const MyCourses = () => {
-    return (
-        <>
-        
-        </>
-    )
-}
+  return (
+    <div className="instructor-course-wrapper">
+        <div className="instructor-course-container">
+            <div className="instructor-course-card">
+                <img src={pic} alt="JavaScript Course" className="instructor-course-image" />
+
+                <div className="instructor-course-body">
+                    <h3 className="instructor-course-title">JavaScript Essentials</h3>
+
+                    <div className="instructor-course-meta">
+                        <span><FaUser className="user-icon"/> 1935 students</span>
+                        <span><FaStar className="star-icon"/> 4.5</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
 
 export default MyCourses;
