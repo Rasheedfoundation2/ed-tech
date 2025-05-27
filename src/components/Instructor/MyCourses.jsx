83b5420db9aa4d86c4b107db0482cbc6d54bb983
arunvRF <arun@rasheedfoundation.org>
2025-05-27 18:55:01 +0530

    mapped courses with courseContext

--- src/components/Instructor/MyCourses.jsx
+++ src/components/Instructor/MyCourses.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CourseContext } from "../../context/CourseContext";
 import '../../css/Instructor/MyCourses.css';
-import pic from "../../assets/images/courseSlider/js.png";
 import { FaStar, FaUser } from "react-icons/fa";
 
 const MyCourses = () => {
-  return (
-    <div className="instructor-course-wrapper">
-        <div className="instructor-course-container">
-            <div className="instructor-course-card">
-                <img src={pic} alt="JavaScript Course" className="instructor-course-image" />
 
-                <div className="instructor-course-body">
-                    <h3 className="instructor-course-title">JavaScript Essentials</h3>
-
-                    <div className="instructor-course-meta">
-                        <span><FaUser className="user-icon"/> 1935 students</span>
-                        <span><FaStar className="star-icon"/> 4.5</span>
+    const {courses} = useContext(CourseContext);
+    console.log(courses)
+    
+    return (
+        <div className="instructor-course-wrapper">
+            <div className="instructor-course-container">
+                {courses.map(course => (
+                    <div key={course.id} className="instructor-course-card">
+                        <img src={course.image} alt="JavaScript Course" className="instructor-course-image" />
+                
+                        <div className="instructor-course-body">
+                            <h3 className="instructor-course-title">{course.title}</h3>
+                
+                            <div className="instructor-course-meta">
+                                <span><FaUser className="instructor-user-icon"/> 1935 students</span>
+                                <span><FaStar className="instructor-star-icon"/> {course.rating}</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default MyCourses;
